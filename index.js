c70f3fbc4f928fcefcfe09e1a47833928439d46a
Sean Altemose <sean@seanaltemose.com>
2019-04-26 14:55:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function getFirstSelector(selector){
   var selectors = document.querySelectorAll(selector);
   return selectors[0]
+};
+
+function nestedTarget(){
+  var nested = document.getElementById('nested');
+  nested.removeChild('target')
+  
 }
